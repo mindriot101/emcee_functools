a35324771557f365d2e901f582c012a33ecaed1e
Simon Walker <s.r.walker101@googlemail.com>
2015-09-06 16:05:36 +0100

    Ensure fixed values cannot change

--- src/emcee_functools/__init__.py
+++ src/emcee_functools/__init__.py
@@ -2,5 +2,14 @@ class Fixed(object):
     '''
     Class describing a parameter which does not vary
     '''
+
     def __init__(self, value):
-        self.value = value
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, *args, **kwargs):
+        raise ValueError("Fixed values cannot change")
--- testing/test_vartype.py
+++ testing/test_vartype.py
@@ -1,6 +1,13 @@
+import pytest
 from emcee_functools import Fixed
 
 
 def test_fixed_can_contain_a_value():
     f = Fixed(5)
     assert f.value == 5
+
+
+def test_fixed_value_does_not_change():
+    f = Fixed(5)
+    with pytest.raises(ValueError) as err:
+        f.value += 5
