83497e6c6ffeb56e567b5ac99e1e866a17c91693
Simon Walker <s.r.walker101@googlemail.com>
2015-09-06 17:54:29 +0100

    Refactor: add Fixed(5) fixture

--- testing/test_vartype.py
+++ testing/test_vartype.py
@@ -2,23 +2,24 @@ import pytest
 from emcee_functools import Fixed
 
 
-def test_fixed_can_contain_a_value():
-    f = Fixed(5)
-    assert f.value == 5
+@pytest.fixture
+def fixed():
+    return Fixed(5)
 
 
-def test_fixed_value_does_not_change():
-    f = Fixed(5)
+def test_fixed_can_contain_a_value(fixed):
+    assert fixed.value == 5
+
+
+def test_fixed_value_does_not_change(fixed):
     with pytest.raises(ValueError) as err:
-        f.value += 5
+        fixed.value += 5
 
 
-def test_fixed_value_does_not_change_at_class_level():
-    f = Fixed(5)
+def test_fixed_value_does_not_change_at_class_level(fixed):
     with pytest.raises(ValueError) as err:
-        f += 5
+        fixed += 5
 
 
-def test_string_representation():
-    f = Fixed(5)
-    assert str(f) == '<Fixed value 5>'
+def test_string_representation(fixed):
+    assert str(fixed) == '<Fixed value 5>'
