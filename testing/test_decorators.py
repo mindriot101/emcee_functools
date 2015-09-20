152f8cf21404683736af0b15657034affc2fa4ff
Simon Walker <s.r.walker101@googlemail.com>
2015-09-20 18:07:31 +0100

    Tests create wrapped function on the fly

--- testing/test_decorators.py
+++ testing/test_decorators.py
@@ -2,15 +2,16 @@ import inspect
 from emcee_functools import func_signature, Fixed, Varying
 
 
-@func_signature(a=Fixed(), b=Varying(), c=Varying())
-def fn(**params):
+def _fn(**params):
     return params
 
 
 def test_parameter_manipulation():
+    fn = func_signature(a=Varying(), b=Fixed())(_fn)
     sig = inspect.signature(fn)
     assert 'p' in sig.parameters
 
 
 def test_values():
+    fn = func_signature(a=Fixed(), b=Varying(), c=Varying())(_fn)
     assert fn([1, 2], a=3) == {'a': 3, 'b': 1, 'c': 2,}
